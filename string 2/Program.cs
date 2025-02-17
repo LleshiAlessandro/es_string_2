df4e62a8989beb769422d2b9c795b744436f304b
lleshi_alessandro <lleshi.alessandro@franchettisalviani.net>
2025-02-17 13:16:00 +0100

    prima versione

---
+++ string 2/Program.cs
@@ -0,0 +1,13 @@
+﻿bool controllo(string esp)
+{
+    string b = esp;
+    for (int i = 0; i < esp.Length; i++)
+    {
+        esp.Contains('(');
+        b.Contains(')');
+    }
+
+}
+
+Console.WriteLine("dimmi la tua espressione matematica ");
+string esp = Console.ReadLine();
